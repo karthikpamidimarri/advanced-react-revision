13d862af710b52d69fb0a0f8fb3e6c98cc29f67d
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-09-16 12:45:15 +0530

    Step 5.6: Configuring babel to run only on js files instead of all node moudles

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 
 const config = {
-    entry: './lib/components/Index.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, use: 'babel-loader' }
-        ]
-    }
+  entry: './lib/components/Index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
 };
 
 module.exports = config;
